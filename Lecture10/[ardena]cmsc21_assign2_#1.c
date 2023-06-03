e3b7256625221b1367abe9a0e50aa92201e4d676
elirvrrii <130275454+elirvrrii@users.noreply.github.com>
2023-06-03 13:22:45 +0800

    Add files via upload

---
+++ Lecture10/[ardena]cmsc21_assign2_#1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h> 
+#include <stdbool.h>
+
+void scan_word(int occurences[26]); //function prototypes
+bool is_anagram(int occurences1[26], int occurences2[26]);
+
+int main(void){ //main function
+    
+    int occurences1[26] = {0};
+    int occurences2[26] = {0};
+    
+    //asks for user input 
+    
+    printf("Enter first word: "); 
+    scan_word(occurences1);
+    
+    printf("Enter second word: ");
+    scan_word(occurences2);
+    
+    if(is_anagram(occurences1, occurences2)){ //checks if function returns true
+        printf("The words are anagrams. \n");
+        return 0;
+    }
+    
+    printf("The words are not anagrams. \n"); //if function returns false
+    return 0;
+    
+}
+
+void scan_word(int occurences[26]){ //function to determine the count of the word in user input
+    
+    char c;
+    
+    while((c = getchar()) != '\n'){
+        if(isalpha(c)){
+            occurences[toupper(c) - 'A']++;
+        }
+    }
+}
+
+bool is_anagram(int occurences1[26], int occurences2[26]){ //function to determine if both words are anagrams
+    
+    int i;
+    
+    for(i = 0; i < 26; i++){
+        if(occurences1[i] != occurences2[i]){ //if counts match, declare false
+            return false;
+            break;
+        }
+    }
+    
+    return true; //otherwise, false
+}
